d35911d37a64fa0d625710788d9818594c73fdf3
JaredAcker <87212179+JaredAcker@users.noreply.github.com>
2021-08-21 18:15:56 -0400

    Update script.js
    created a function for the timer

--- script.js
+++ script.js
@@ -62,3 +62,17 @@ const questions = [
         correctAnswer: "1"
     }
 ];
+
+function setTime() {
+    let timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = `Time:${secondsLeft}s`;
+
+        if (secondsLeft === 0 || questionCount === questions.length) {
+            clearInterval(timerInterval);
+            questionsEl.style.display = "none";
+            finalEl.style.display = "block";
+            scoreEl.textContent = secondsLeft;
+        }
+    }, 1000);
+}
