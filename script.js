fdd0284caca11ac5e48184c513200abd6b50989f
JaredAcker <87212179+JaredAcker@users.noreply.github.com>
2021-08-21 15:14:21 -0400

    Update script.js
    adding in more query selectors for the intro and the questions

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 let timeEl = document.querySelector("p.time");
 let secondsLeft = 75;
 let scoreEl = document.querySelector("#score");
+
+// adding more query selectors for the intro and the questions
+const introEl = document.querySelector("#intro");
+
+const questionsEl = document.querySelector("#questions");
+let questionEl = document.querySelector("#question");
+let questionCount = 0;
+const yaynayEl = document.querySelector("#yesno");
