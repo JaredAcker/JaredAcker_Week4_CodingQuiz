d5e3c04c8a0cfbaebe6d2ce2c95911e45b0024a7
JaredAcker <87212179+JaredAcker@users.noreply.github.com>
2021-08-21 15:29:19 -0400

    Update script.js
    added in query selectors for the start button, answers 1-4 buttons, submit score button, the go back button, clear scores button and view scores button

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ let questionEl = document.querySelector("#question");
 let questionCount = 0;
 const yesnoEl = document.querySelector("#yesno");
 
+// starts the quiz
+const startBtn = document.querySelector("#start");
+
+// buttons
+const ansBtn = document.querySelectorAll("button.ansBtn")
+const ans1Btn = document.querySelector("#answer1");
+const ans2Btn = document.querySelector("#answer2");
+const ans3Btn = document.querySelector("#answer3");
+const ans4Btn = document.querySelector("#answer4");
+const submitScrBtn = document.querySelector("#submit-score");
+const goBackBtn = document.querySelector("#goback");
+const clearScrBtn = document.querySelector("#clearscores");
+const viewScrBtn = document.querySelector("#view-scores");
