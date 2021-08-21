7173e8b5a203805754c77b255e49d68e382ecb81
JaredAcker <87212179+JaredAcker@users.noreply.github.com>
2021-08-21 15:37:05 -0400

    Update script.js
    section houses the questions that are shown on the buttons in the html

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ const highscoresEl = document.querySelector("#highscores");
 let scoreListEl = document.querySelector("#score-list");
 let scoreList = [];
 
+// section that has query selectors for the final part where the quiz taker can input their initials 
+const finalEl = document.querySelector("#final");
+let initialsInput = document.querySelector("#initials");
+
 // starts the quiz
 const startBtn = document.querySelector("#start");
 
@@ -29,3 +33,32 @@ const submitScrBtn = document.querySelector("#submit-score");
 const goBackBtn = document.querySelector("#goback");
 const clearScrBtn = document.querySelector("#clearscores");
 const viewScrBtn = document.querySelector("#view-scores");
+
+// section houses the questions that are shown on the buttons in the html
+const questions = [
+    {
+        question: "Arrays in Javascript can be used to store ____.",
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer: "3"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correctAnswer: "3"
+    },
+    {
+        question: "Commonly used data types do NOT include:",
+        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        correctAnswer: "2"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        correctAnswer: "2"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        correctAnswer: "1"
+    }
+];
